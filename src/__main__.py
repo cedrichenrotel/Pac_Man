d138181db8c295e8f7fc24db69ee04d0e90a0766
cedrichenrotel <cedric.henrotel@gmail.com>
2026-08-17 10:39:33 +0200

    create main

---
+++ src/__main__.py
@@ -0,0 +1,10 @@
+def main() -> None:
+	try:
+		"pac_man"
+	except KeyboardInterrupt:
+		print("[WARNING]: The programme was stopped manually\n")
+		sys.exit()
+
+
+if __name__ == "__main__":
+	main()
