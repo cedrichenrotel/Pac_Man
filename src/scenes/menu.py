71a270b09c12d217c719233b13eaef66e4019882
ewa <mateo.gibert@epitech.eu>
2026-08-20 12:42:41 +0200

    logo centrer avec laide de pillow , suppression dune ligne dans gitignore, ajout de la doc mlx

--- src/scenes/menu.py
+++ src/scenes/menu.py
@@ -1,5 +1,7 @@
 from src.scenes.base import Scene
 from src.utils import make_color
+from PIL import Image
+import os
 
 red = make_color(255, 0, 0, 0)
 green = make_color(0, 255, 0, 0)
@@ -8,6 +10,7 @@ black = make_color(0, 0, 0, 255)
 LIGHT_GRAY = make_color(200, 200, 200)
 GRAY = make_color(128, 128, 128)
 DARK_GRAY = make_color(60, 60, 60)
+YELLOW = make_color(255, 255, 0)
 
 
 class MenuScene(Scene):
@@ -23,60 +26,54 @@ class MenuScene(Scene):
         self.middle_h = int(self.height / 2) - 100
         self.step = 40
 
-    def write_banner(self, width, height):
-        for i in range(1, 150):
-            self.mlx.mlx_pixel_put(self.mlx_init, self.mlx_window, width + i,
-                                   height, GRAY)
-            self.mlx.mlx_pixel_put(self.mlx_init, self.mlx_window, width + i,
-                                   height + 20, GRAY)
+    # def write_banner(self, width, height):
+    #     for i in range(1, self.step):
+    #         self.mlx.mlx_pixel_put(self.mlx_init, self.mlx_window,
+    #                                width, height + i, GRAY)
+    #     for i in range(1, 150):
+    #         self.mlx.mlx_pixel_put(self.mlx_init, self.mlx_window, width + i,
+    #                                height, GRAY)
+    #         self.mlx.mlx_pixel_put(self.mlx_init, self.mlx_window, width + i,
+    #                                height + 20, GRAY)
 
     def write_title(self):
-        self.write_banner(self.middle_w, self.middle_h)
+        # self.write_banner(self.middle_w, self.middle_h)
         self.mlx.mlx_string_put(self.mlx_init, self.mlx_window, self.middle_w,
-                                self.middle_h, 255, "Start Game")
+                                self.middle_h, YELLOW, "Start Game")
 
-        self.write_banner(self.middle_w, self.middle_h + self.step)
+        # self.write_banner(self.middle_w, self.middle_h + self.step)
         self.mlx.mlx_string_put(self.mlx_init, self.mlx_window, self.middle_w,
                                 self.middle_h + self.step,
-                                255, "View Highscores")
+                                YELLOW, "View Highscores")
 
-        self.write_banner(self.middle_w, self.middle_h + self.step*2)
+        # self.write_banner(self.middle_w, self.middle_h + self.step*2)
         self.mlx.mlx_string_put(self.mlx_init, self.mlx_window, self.middle_w,
                                 self.middle_h + self.step*2,
-                                255, "Instructions")
+                                YELLOW, "Instructions")
 
-        self.write_banner(self.middle_w, self.middle_h + self.step*3)
+        # self.write_banner(self.middle_w, self.middle_h + self.step*3)
         self.mlx.mlx_string_put(self.mlx_init, self.mlx_window, self.middle_w,
-                                self.middle_h + self.step*3, red,
+                                self.middle_h + self.step*3, YELLOW,
                                 "Exit")
 
+    def install_menu_image(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        image_path = os.path.join(current_dir, "./../../assets/menu/menu_logo.png")
+        image_path = os.path.normpath(image_path)
+        Image.open(image_path).convert("RGBA").save(image_path)
+
+        self.img = self.mlx.mlx_png_file_to_image(self.mlx_init, image_path)
+        img_ptr, img_width, img_height = self.img
+
+        left_space = self.width - img_width
+
+        if img_ptr:
+            self.mlx.mlx_put_image_to_window(self.mlx_init, self.mlx_window,
+                                             img_ptr, int(left_space / 2), 0)
+
     def launch(self):
-        self.get_calc()
-        self.write_title()
         self.mlx.mlx_clear_window(self.mlx_init, self.mlx_window)
-
-
-# def mymouse(button, x, y, mystuff):
-#     print(f"Got mouse event! button {button} at {x},{y}.")
-
-
-# def mykey(keynum, mystuff):
-#     print(f"Got key {keynum}, and got my stuff back:")
-#     print(mystuff)
-#     if keynum == 32:
-#         m.mlx_mouse_hook(win_ptr, None, None)
-
-# from mlx import Mlx
-# m = Mlx()
-# mlx_ptr = m.mlx_init()
-# win_ptr = m.mlx_new_window(mlx_ptr, 200, 200, "toto")
-# m.mlx_clear_window(mlx_ptr, win_ptr)
-# m.mlx_string_put(mlx_ptr, win_ptr, 20, 20, 255, "Hello PyMlx!")
-# # (ret, w, h) = m.mlx_get_screen_size(mlx_ptr)
-# # print(f"Got screen size: {w} x {h} .")
-
-# # stuff = [1, 2]
-# # m.mlx_mouse_hook(win_ptr, mymouse, None)
-# # m.mlx_key_hook(win_ptr, mykey, stuff)
-
-# m.mlx_loop(mlx_ptr)
+        self.get_calc()
+        self.install_menu_image()
+        self.write_title()
+        self.mlx.mlx_do_sync(self.mlx_init)
